44a258d6d728457d626d53fc0e47d7632aedeed5
Deval Shah <devalshah40@gmail.com>
2017-03-29 09:48:55 +0530

    Commit booking

--- demo.sql
+++ demo.sql
@@ -12,11 +12,98 @@ MySQL - 10.1.13-MariaDB : Database - yii1blog
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `booking` */
+
+DROP TABLE IF EXISTS `booking`;
+
+CREATE TABLE `booking` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `yatrik_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `city` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `pincode` char(7) COLLATE utf8_unicode_ci NOT NULL,
+  `mobile_no` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `arrival_date` date NOT NULL,
+  `departure_date` date NOT NULL,
+  `receipt_no` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `deposit_amount` decimal(10,2) NOT NULL,
+  `actual_amount` decimal(10,2) NOT NULL,
+  `notes` text COLLATE utf8_unicode_ci,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `booking` */
+
+/*Table structure for table `booking_details` */
+
+DROP TABLE IF EXISTS `booking_details`;
+
+CREATE TABLE `booking_details` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `booking_id` int(11) NOT NULL,
+  `room_id` int(11) DEFAULT NULL,
+  `number_count` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `booking_details` */
+
+/*Table structure for table `rooms` */
+
+DROP TABLE IF EXISTS `rooms`;
+
+CREATE TABLE `rooms` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `room_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `room_price` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `room_info` text COLLATE utf8_unicode_ci,
+  `room_capacity` int(11) NOT NULL DEFAULT '0',
+  `room_status` tinyint(1) DEFAULT '1',
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `rooms` */
+
+insert  into `rooms`(`id`,`room_name`,`room_price`,`room_info`,`room_capacity`,`room_status`,`created_date`,`updated_date`) values (1,'AC room','300.00','AC rooms',4,1,'2017-03-27 22:39:55','2017-03-27 22:39:55'),(2,'Non AC room','300.00','AC rooms',4,1,'2017-03-27 22:39:55','2017-03-27 22:39:55'),(3,'Non AC room','300.00','AC rooms',4,1,'2017-03-27 22:39:55','2017-03-27 22:39:55'),(4,'Non AC room','300.00','AC rooms',4,1,'2017-03-27 22:39:55','2017-03-27 22:39:55'),(5,'Non AC room','300.00','AC rooms',4,1,'2017-03-27 22:39:55','2017-03-27 22:39:55');
+
 /*Table structure for table `users` */
 
 DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `email` varchar(128) NOT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `mobile_no` varchar(255) DEFAULT NULL,
+  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `lastvisit_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `status` int(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`),
+  KEY `status` (`status`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`username`,`password`,`first_name`,`last_name`,`email`,`location`,`mobile_no`,`create_at`,`lastvisit_at`,`status`) values (1,'admin','21232f297a57a5a743894a0e4a801fc3','Deval','Shah','devalshah21@gmail.com','Ahmedabad','7874734209','2017-03-28 22:53:36','0000-00-00 00:00:00',1),(4,'demo','fe01ce2a7fbac8fafaed7c982a04e229','Admin','Shah','demo@example.com','Delhi','9274305014','2017-03-28 22:53:59','0000-00-00 00:00:00',0);
+
+/*Table structure for table `users_old` */
+
+DROP TABLE IF EXISTS `users_old`;
+
+CREATE TABLE `users_old` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) NOT NULL,
   `password` varchar(128) NOT NULL,
@@ -32,9 +119,9 @@ CREATE TABLE `users` (
   KEY `status` (`status`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
-/*Data for the table `users` */
+/*Data for the table `users_old` */
 
-insert  into `users`(`id`,`username`,`password`,`email`,`location`,`mobile_no`,`create_at`,`lastvisit_at`,`status`) values (1,'admin','21232f297a57a5a743894a0e4a801fc3','devalshah21@gmail.com',NULL,NULL,'2017-03-22 23:19:36','2017-03-22 20:01:31',1),(2,'demo','fe01ce2a7fbac8fafaed7c982a04e229','demo@example.com',NULL,NULL,'2017-03-22 23:19:36','2017-03-22 20:00:48',1);
+insert  into `users_old`(`id`,`username`,`password`,`email`,`location`,`mobile_no`,`create_at`,`lastvisit_at`,`status`) values (1,'admin','21232f297a57a5a743894a0e4a801fc3','devalshah21@gmail.com',NULL,NULL,'2017-03-22 23:19:36','2017-03-22 20:01:31',1),(2,'demo','fe01ce2a7fbac8fafaed7c982a04e229','demo@example.com',NULL,NULL,'2017-03-22 23:19:36','2017-03-22 20:00:48',1);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
